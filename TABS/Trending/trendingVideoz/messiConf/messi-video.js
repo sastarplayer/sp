dd22cb5f6a8b28f8fd4a96bfe35a60c8f01d9117
sastarplayer <83297404+sastarplayer@users.noreply.github.com>
2021-08-11 14:59:18 +0200

    commit to main

---
+++ TABS/Trending/trendingVideoz/messiConf/messi-video.js
@@ -0,0 +1,14 @@
+var myvid = document.getElementById('myvideo');
+var myvids = [ 
+"/sp/TABS/Trending/videoz/videopsg.mp4",
+  ];
+var activeVideo = 0;
+/TABS/Entertainment/videos/mamelodi-sundowns.mp4
+myvid.addEventListener('ended', function(e) {
+  // update the active video index
+  activeVideo = (++activeVideo) % myvids.length;
+
+  // update the video source and play
+  myvid.src = myvids[activeVideo];
+  myvid.play();
+});
